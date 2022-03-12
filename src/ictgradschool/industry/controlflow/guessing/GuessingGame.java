5e2ab0c569958dfec0b6380bcfd902cbc664e229
Taneya <twil609@aucklanduni.ac.nz>
2022-03-12 11:41:19 +1000

    Activity 7 complete

--- src/ictgradschool/industry/controlflow/guessing/GuessingGame.java
+++ src/ictgradschool/industry/controlflow/guessing/GuessingGame.java
@@ -1,5 +1,7 @@
 package ictgradschool.industry.controlflow.guessing;
 
+import ictgradschool.Keyboard;
+
 /**
  * A guessing game!
  */
@@ -8,7 +10,31 @@ public class GuessingGame {
     public void start() {
 
         // TODO Write your code here.
-    }
+
+            int min = 1;
+            int max = 100;
+            int goal = (int)(Math.random() * (max-min+1)) + min;
+
+            int guess = 0;
+            while (guess != goal) {
+
+                System.out.println("Enter your guess between 1 and 100: ");
+                guess = Integer.parseInt(Keyboard.readInput());
+
+                if (guess < goal) {
+                    System.out.println("Your number is too low, try again");
+                }
+                else if (guess > goal) {
+                    System.out.println("Your number is too high, try again");
+                }
+                else {
+                    System.out.println("Perfect!");
+                }
+
+            }
+
+            System.out.println("Game Over");
+        }
 
     /**
      * Program entry point. Do not edit.
